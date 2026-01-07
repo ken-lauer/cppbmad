d2151b4893c68bd7c4d0eaf13a052b9f0dcfbf99
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-01-06 16:17:26 -0800

    WIP: first pass at CI

--- regenerate.sh
+++ regenerate.sh
@@ -9,7 +9,7 @@ build_type=${1:-debug}
 
 python -m codegen &&
   cmake -DCMAKE_BUILD_TYPE="${build_type}" -B "${build_type}" . &&
-  make -j 8 -C "${build_type}" &&
+  make -j8 -C "${build_type}" &&
   ./$build_type/test_all_encompassing &&
   ./$build_type/test_integration &&
   ./$build_type/test_arrays &&
