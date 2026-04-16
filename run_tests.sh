adc111d628950586cbb6078697a9d0284d44004e
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-04-16 09:58:05 -0700

    TST: move all tests into a unified cppbmad_tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,6 @@
 
 build_type=${1:-debug}
 
-./$build_type/test_all_encompassing &&
-  ./$build_type/test_integration &&
-  ./$build_type/test_arrays &&
-  ./$build_type/test_csr &&
+./$build_type/cppbmad_tests &&
   (cd python/examples/ && bash run_examples.sh) &&
   (cd python/tests/ && python -m pytest)
--- src/tests/test_csr.cpp
+++ src/tests/test_csr.cpp
@@ -1,4 +1,3 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <bmad.hpp>
 
 #include <fstream>
